e28afb2c78e704f70851afcfa7c65d01b6ad2667
Miguel Guerra <migueclase2@gmail.com>
2016-11-03 11:14:35 +0100

    0456.a, la maquina ahora puede ser premium

--- TicketMachine.java
+++ TicketMachine.java
@@ -17,15 +17,20 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
-
+    // The chance of getting a prize
+    private int prize;
+    //Maximum number of tickets that the machine is allowed to sell before error
+    private int maxbilletes;
     /**
      * Create a machine that issues tickets of the given price.
      */
-    public TicketMachine(int cost)
+    public TicketMachine(int cost, int premium,int billetesmaximos)
     {
         price = cost;
         balance = 0;
         total = 0;
+        prize = premium;
+        maxbilletes = billetesmaximos;
     }
 
     /**
@@ -67,7 +72,7 @@ public class TicketMachine
      */
     public void printTicket()
     {
-        if(balance >= price) {
+    if (prize == 1) {
             // Simulate the printing of a ticket.
             System.out.println("##################");
             System.out.println("# The BlueJ Line");
@@ -75,17 +80,30 @@ public class TicketMachine
             System.out.println("# " + price + " cents.");
             System.out.println("##################");
             System.out.println();
-
+            
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket");
+            System.out.println("# " + price + " cents.");
+            System.out.println("##################");
+            System.out.println();
+            
             // Update the total collected with the price.
             total = total + price;
             // Reduce the balance by the prince.
             balance = balance - price;
         }
-        else {
-            System.out.println("You must insert at least: " +
-                               (price - balance) + " more cents.");
-                    
+        else if (prize == 0) {
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket");
+            System.out.println("# " + price + " cents.");
+            System.out.println("##################");
+            System.out.println();
         }
+        else {
+            System.out.println ("Error , premium can only be 1(if activated) or 0 (if desactivated)");
+}
     }
 
     /**
